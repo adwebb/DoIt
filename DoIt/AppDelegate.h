58eee84edd3a82d509a2e18ad2c70ff693113af6
Andrew Webb <adwebb@gmail.com>
2014-01-13 13:32:50 -0600

    Initial Commit

---
+++ DoIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DoIt
+//
+//  Created by Andrew Webb on 1/13/14.
+//  Copyright (c) 2014 Andrew Webb and Poulose Matthen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DoIt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DoIt
+//
+//  Created by Andrew Webb on 1/13/14.
+//  Copyright (c) 2014 Andrew Webb and Poulose Matthen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
